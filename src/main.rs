a88091f6654c2ce00c482d0d5905520ac912a601
Jacob <jacob@notactivelylooking.com>
2023-09-17 18:30:46 +0100

    Updating demo test.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,28 @@ pub mod board;
 use board::Board;
 
 fn main() {
-    let mut board = Board::randomise(4, 3);
+    // let mut board = Board::randomise(4, 3);
+    let mut board = Board::from_vec(
+        vec![
+            board::CellState::Dead,
+            board::CellState::Alive,
+            board::CellState::Dead,
+            board::CellState::Dead,
+            board::CellState::Alive,
+            board::CellState::Alive,
+            board::CellState::Dead,
+            board::CellState::Alive,
+            board::CellState::Dead,
+            board::CellState::Dead,
+            board::CellState::Alive,
+            board::CellState::Dead,
+        ],
+        Some(3),
+    );
     println!("{}", board);
-    // board.step();
+    println!("x_size: {}", board.x_size);
+    println!("y_size: {}", board.y_size);
     for index in 0..board.size() {
         board.get_neighbours(index);
-        // println!("{}", board.get_neighbours(index));
     }
-    // for v in [0, 1, 3, 5, 6, 10] {
-    //     println!("{}", board.get_neighbours(v));
-    // }
 }
