597be24361107d07b442be6352997d8b4ddfb4fb
Jacob <jacob@notactivelylooking.com>
2023-09-17 15:18:48 +0100

    Updating demo test.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ fn main() {
     let mut board = Board::randomise(4, 3);
     println!("{}", board);
     // board.step();
-    for v in [0, 1, 3, 5, 6, 10] {
-        println!("{}", board.get_neighbours(v));
+    for index in 0..board.size() {
+        board.get_neighbours(index);
+        // println!("{}", board.get_neighbours(index));
     }
+    // for v in [0, 1, 3, 5, 6, 10] {
+    //     println!("{}", board.get_neighbours(v));
+    // }
 }
